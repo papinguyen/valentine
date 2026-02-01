890f585a6883abcb4dbaf802fbb34ad0b089ee86
papinguyen <vvtvm8szzs@privaterelay.appleid.com>
2026-01-31 22:46:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@
 
 const CONFIG = {
     // Your Valentine's name that will appear in the title
-    // Example: "Jade", "Sarah", "Mike"
+    // Example: "Stephanie"
     valentineName: "Tuti",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Will You Be My Valentine? 🥰",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -24,7 +24,7 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "I don't just like you, I LOVE you! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
@@ -32,7 +32,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th? 🌹", // The big question!
             yesBtn: "HAI!",                                             // Text for "Yes" button
             noBtn: "Không..."                                                 // Text for "No" button
         }
@@ -41,14 +41,14 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "Oh wow!! You love me that much?? 🥰😘😘",  // Shows when they go past 5000%
+        high: "You're the best! We can do more! 😘",              // Shows when they go past 1000%
+        normal: "Not enough! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
+        title: "Happy Valentine's Day BB, can't wait to celebrate with you!",
          gif: "togepipikachuhug.gif"
     },
  
